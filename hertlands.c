33200cd54ffa6da52496e7ed2d3d54ff7166f072
CauaBrawlstars <caua53589@gmail.com>
2024-06-25 11:12:14 -0300

    add arcem c

---
+++ hertlands.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+
+printf("Helo word Bound to fall in love Bound to fall in love uhhuh honeyAll them other niggas lame and you know it nowWhen a real nigga hold you down alright you s pposed to drownBound bound to fall in loveBound boun to fall in love uhhu honey doing in the club on a ThursdayShe say she only here  her girl birthdayThey ordered champagne but still look thirstyRock Forever 21 but just turned thirty");
+
+return 0;
+}
